86ce0541aa0585152a7a1b2a6f5d89ecd12d93e2
origin <https://github.com/AdsonSantiago/Test-curso-dio-dominando-IDES-java-intellij.git>
2026-09-20 18:45:07 -0300

    variaveis de ambiente para vercel2

--- financas/urls.py
+++ financas/urls.py
@@ -7,8 +7,22 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 
+from django.http import JsonResponse
+import os
+
+def debug_env(request):
+    return JsonResponse({
+        "SECRET_KEY_present": bool(os.getenv("SECRET_KEY")),
+        "DEBUG": os.getenv("DEBUG", "não definido"),
+        "DATABASE_URL_present": bool(os.getenv("DATABASE_URL")),
+        "enviroment": "Vercel",
+    })
+
+
+
 
 urlpatterns = [
+    path('debug-env/', debug_env),
     path("admin/", admin.site.urls),
 
     path("api/financeiro/", include("apps.financeiro.urls")),
