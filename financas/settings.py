df0c7a74758902032ee31429c7c054b2efc2bfd9
origin <https://github.com/AdsonSantiago/Test-curso-dio-dominando-IDES-java-intellij.git>
2026-09-22 20:55:21 -0300

    config: django para amb produ 2

--- financas/settings.py
+++ financas/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     "apps.authentication",
     'corsheaders',
 
-    'apps.relatorios',
+    # 'apps.relatorios',
     'apps.usuarios',
     'apps.financeiro',
     'apps.core',
